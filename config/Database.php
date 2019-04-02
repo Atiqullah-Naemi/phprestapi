2fbf19269a6fa01f5b5f55b1c80a97b7f1ed80c7
atiqullah-naemi <atiqullah.naemi21@gmail.com>
2019-04-02 19:31:21 +1100

    first commit

---
+++ config/Database.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * This class is used to connect to database 
+ */
+class Database
+{
+	private $dbhost = 'localhost';
+	private $dbname = 'phpapi';
+	private $dbusername = 'root';
+	private $dbpassword = '';
+	private $con;
+}
+
+
+?>
